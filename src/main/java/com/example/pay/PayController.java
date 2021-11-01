493d152ee0d127817d6424f31803702a2b983557
IvanKustarev <ivan.kustarev@gmail.com>
2021-11-01 10:29:34 +0300

    finish 0.1

--- src/main/java/com/example/pay/PayController.java
+++ src/main/java/com/example/pay/PayController.java
@@ -8,15 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.util.DateUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 
 @Controller
-@RequestMapping("/pay/")
+@RequestMapping("/pay")
 public class PayController {
+
+    @PostConstruct
+    private void startAutoPayer(){
+        AutoPay autoPay = new AutoPay(subscriptionsRepository, usersRepository);
+        autoPay.start();
+    }
 
     @Autowired
     private SubscriptionsRepository subscriptionsRepository;
@@ -28,6 +34,24 @@ public class PayController {
     public String showPay(@PathVariable("id") Long userId, Model model){
         Users user = usersRepository.findById(userId).get();
         model.addAttribute("balance", user.getBalance());
+//        Iterator<Subscriptions> subscriptionsIterator = subscriptionsRepository.findAll().iterator();
+//        Subscriptions subscriptions = null;
+//        while (subscriptionsIterator.hasNext()){
+//            subscriptions = subscriptionsIterator.next();
+//            if(subscriptions.getUsedBy().equals(userId)){
+//                break;
+//            }
+//            subscriptions = null;
+//        }
+//        if(subscriptions != null) {
+//            String aboutSub = "Автопродление подписки: " + subscriptions.getAutoPay();
+//            model.addAttribute("aboutSub", aboutSub);
+//        }else {
+//
+//        }
+
+        addModelAttributes(model, userId);
+
         return "pay";
     }
 
@@ -38,6 +62,9 @@ public class PayController {
         usersRepository.save(users);
         model.addAttribute("message", "Средства начислены!");
         model.addAttribute("toMainURL", "/pay/" + userId + "/");
+
+        addModelAttributes(model, userId);
+
         return "message";
     }
 
@@ -47,6 +74,9 @@ public class PayController {
         if(user.getBalance()/5 < monthsNumber){
             model.addAttribute("message", "Недостаточно средст!");
             model.addAttribute("toMainURL", "/pay/" + userId + "/");
+
+            addModelAttributes(model, userId);
+
             return "message";
         }else {
             user.setBalance(user.getBalance() - monthsNumber*5);
@@ -62,10 +92,11 @@ public class PayController {
             }
             if(subscriptions == null){
                 subscriptions = new Subscriptions();
+                subscriptions.setUsedBy(userId);
                 subscriptions.setStartTime(new Date().getTime());
                 subscriptions.setEndTime(new Date().getTime());
+                subscriptions.setAutoPay(true);
             }
-
             Date dt = new Date(subscriptions.getEndTime());
             Calendar c = Calendar.getInstance();
             c.setTime(dt);
@@ -75,7 +106,20 @@ public class PayController {
 
             model.addAttribute("message", "Подписка продлена!");
             model.addAttribute("toMainURL", "/pay/" + userId + "/");
+
+            addModelAttributes(model, userId);
+
             return "message";
         }
+    }
+
+    private Model addModelAttributes(Model model, Long userId){
+        model.addAttribute("toAuthorisation", "http://localhost:8081/authorisation/");
+        model.addAttribute("toProfile", "http://localhost:8082/profile/" + userId + "/");
+        model.addAttribute("toCatalog", "http://localhost:8083/catalog/" + userId + "/");
+        model.addAttribute("toSubscription", "http://localhost:8084/subscription/" + userId + "/");
+        model.addAttribute("toPay", "http://localhost:8085/pay/" + userId + "/");
+        model.addAttribute("toPenalties", "http://localhost:8086/penalties/" + userId + "/");
+        return model;
     }
 }
